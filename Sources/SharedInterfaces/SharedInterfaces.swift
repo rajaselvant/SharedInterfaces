855dcfab1aa495fe756bbc80b0efe5721e9dabe0
Rajaselvan <rajaselvant@yahoo.com>
2025-05-21 21:08:53 +0530

    first commit

--- Sources/SharedInterfaces/SharedInterfaces.swift
+++ Sources/SharedInterfaces/SharedInterfaces.swift
@@ -1,2 +1,24 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+// Protocol for feature modules
+public protocol FeatureModule {
+    associatedtype Content
+    var featureName: String { get }
+    var featureIcon: String { get }
+    var featureView: Content { get }
+}
+
+// Protocol for communication: host -> feature
+public protocol FeatureInput {
+    associatedtype Input
+    func configure(with input: Input)
+}
+
+// Protocol for communication: feature -> host via callbacks
+public protocol FeatureActionHandler {
+    associatedtype Action
+    func handle(action: Action)
+}
